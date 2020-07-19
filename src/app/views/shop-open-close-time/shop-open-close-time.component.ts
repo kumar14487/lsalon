7200088932f494b23c20080f2b35bbc14cdd5bcb
nandha <nandhacse2010@gmail.com>
2020-07-19 15:21:31 +0530

    Time picker issues

--- src/app/views/shop-open-close-time/shop-open-close-time.component.ts
+++ src/app/views/shop-open-close-time/shop-open-close-time.component.ts
@@ -17,12 +17,19 @@ import { OwlDateTimeModule, OwlNativeDateTimeModule } from 'ng-pick-datetime';
 })
 export class ShopOpenCloseTimeComponent implements OnInit {
   dayNames = [];
+  startTime;
+  endTime;
   constructor() { }
   ngOnInit(): void {
     this.dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   }
   save(){
-    
+    this.startTime = new Date(this.startTime);
+    this.endTime = new Date(this.endTime);
+    this.startTime.setHours(this.startTime.getHours() - 12 );
+    this.startTime = this.startTime.getHours()+":"+this.startTime.getMinutes()+":"+this.startTime.getSeconds();
+    this.endTime = this.endTime.getHours()+":"+this.endTime.getMinutes()+":"+this.endTime.getSeconds();
+    console.log(this.startTime+" ------------------- "+this.endTime);
   }
 
 }
